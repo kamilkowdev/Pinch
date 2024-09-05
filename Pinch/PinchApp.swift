c421490cb3140f44552361a0446a38fec7726f60
kamil <kamilkoww@gmail.com>
2024-09-05 16:13:33 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Kamil Kowalczyk on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
